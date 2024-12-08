9bf05985d6bedee31f8cb532f6c51ba288f79e66
PoyarkovA <mr.sanya.poyarkov@gmail.com>
2024-12-08 21:10:09 +0500

    Update element.py

--- ElementПоярков/element.py
+++ ElementПоярков/element.py
@@ -1,1 +1,24 @@
+class ElementПоярков:
+    def __init__(self, name, symbol, number):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @property
+    def number(self):
+        return self.__number
+
+    def dump(self):
+        print(f"Name: {self.name}, Symbol: {self.symbol}, Number: {self.number}")
+
+phosphor = ElementПоярков("Phosphor", "P", 15)
+phosphor.dump()
 
